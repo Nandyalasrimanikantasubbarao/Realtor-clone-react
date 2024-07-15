ed8d4733c787de5a7dbeaac64714799007ebd584
Nandyala Sri Manikanta Subbarao <srimaninandyala.com>
2024-07-15 16:11:56 +0530

    Create pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import FrogotPassword from "./pages/FrogotPassword";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/Offers" element={<Offers />}></Route>
+          <Route path="/Signin" element={<Signin />}></Route>
+          <Route path="/Signup" element={<Signup />}></Route>
+          <Route path="/Forgot-Password" element={<FrogotPassword />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
